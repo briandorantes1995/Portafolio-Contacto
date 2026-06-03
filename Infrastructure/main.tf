9c4b03f3f7f4696b5a1fd1181aa21344ad9db70b
briandorantes <briandorantes@hotmail.com>
2026-06-03 15:27:12 -0600

    Cambios de proveedor cloud

--- Infrastructure/main.tf
+++ Infrastructure/main.tf
@@ -105,7 +105,7 @@ output "server_ip" {
   value = hcloud_server.node1.ipv4_address
 }
 
-# Create Ansible inventory.ini for running test
+# Create Ansible inventory.ini
 resource "local_file" "deploy_inventory" {
   content = <<EOF
 [servers]
